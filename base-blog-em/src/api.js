19c426e4ecfd98968fa9585a909b7cb7f9807bb8
jun <znznun@gmail.com>
2024-06-19 16:59:59 +0900

    base-blog-em complete

--- base-blog-em/src/api.js
+++ base-blog-em/src/api.js
@@ -1,6 +1,6 @@
-export async function fetchPosts(pageNum = 1) {
+export async function fetchPosts(currentPage) {
   const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${currentPage}`
   );
   return response.json();
 }
